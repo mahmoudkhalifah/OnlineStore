3b00a6be27969cb26f905c082c9f4643ca66f8c3
Mahmoud khalifa <eng.mahmoudkhalifah@gmail.com>
2023-04-02 01:53:50 +0200

    fixed mail issue

--- OnlineStore/RepoServices/EmailSender.cs
+++ OnlineStore/RepoServices/EmailSender.cs
@@ -18,7 +18,7 @@ namespace OnlineStore.RepoServices
         }
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var apiKey = config["SendAPIKey"];
+            /*var apiKey = config["SendAPIKey"];
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("mariam.hesham@yahoo.com");
           //  var subjectt = "Sending with SendGrid is Fun";
@@ -26,7 +26,7 @@ namespace OnlineStore.RepoServices
           //  var plainTextContent = "and easy to do anywhere, even with C#";
             //var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, htmlMessage, htmlMessage);
-            var response = await client.SendEmailAsync(msg);
+            var response = await client.SendEmailAsync(msg);*/
             /* var frommail = "mariamhesham120@​outlook.com";
              var frompassword = "onlinestore###";
              var message = new MailMessage();
@@ -42,6 +42,19 @@ namespace OnlineStore.RepoServices
                  EnableSsl = true
              };
              smtpClient.Send(message);*/
+            var frommail = "onlinestoremvc@gmail.com";
+            var frompassword = "cwjbnievtouquwqe";
+            var message = new MailMessage();
+            message.From = new MailAddress(frommail);
+            message.Subject = subject;
+            message.To.Add(email);
+            message.Body = $"<html><body>{htmlMessage}</body></html>";
+            message.IsBodyHtml = true;
+            SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = new NetworkCredential(frommail, frompassword);
+            smtpClient.EnableSsl = true;
+            smtpClient.Send(message);
         }
     }
 }
