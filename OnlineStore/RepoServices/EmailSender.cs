9f60858fd1c90ca8be544b8571e1eea5d98a7ee5
Mariam Hesham <mariamhesham503@gmail.com>
2023-04-02 02:06:42 +0200

    sendMail

--- OnlineStore/RepoServices/EmailSender.cs
+++ OnlineStore/RepoServices/EmailSender.cs
@@ -2,9 +2,12 @@
 using SendGrid.Helpers.Mail;
 using SendGrid;
 using System.Net;
-using System.Net.Mail;
 using System;
 using System.Threading.Tasks;
+using MimeKit.Text;
+using MimeKit;
+using System.Net.Mail;
+using Microsoft.Identity.Client.Platforms.Features.DesktopOs.Kerberos;
 
 namespace OnlineStore.RepoServices
 {
@@ -18,30 +21,50 @@ namespace OnlineStore.RepoServices
         }
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            var apiKey = config["SendAPIKey"];
+          /*  //rasheed.kub80@ethereal.email
+            var mail = new MimeMessage();
+            mail.From.Add(MailboxAddress.Parse("rasheed.kub80@ethereal.email"));
+            mail.To.Add(MailboxAddress.Parse(email));
+            mail.Subject=subject;
+            mail.Body = new TextPart(TextFormat.Html) { Text = htmlMessage};
+
+            using var smtp = new SmtpClient();
+            smtp.Connect("smtp.ethereal.email",587,MailKit.Security.SecureSocketOptions.StartTls);
+            smtp.Authenticate("rasheed.kub80@ethereal.email", "2JXhyGDWgTEHwqxBQb");
+            smtp.SendAsync(mail);
+            smtp.Disconnect(true);
+            */
+         /*       var apiKey = config["SendAPIKey"];
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("mariam.hesham@yahoo.com");
-          //  var subjectt = "Sending with SendGrid is Fun";
+         //   var from = new EmailAddress("rasheed.kub80@ethereal.email");
+
+            var from = new EmailAddress("mariamhesham503@gmail.com");
+            //  var subjectt = "Sending with SendGrid is Fun";
             var to = new EmailAddress(email);
           //  var plainTextContent = "and easy to do anywhere, even with C#";
             //var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, htmlMessage, htmlMessage);
-            var response = await client.SendEmailAsync(msg);
-            /* var frommail = "mariamhesham120@​outlook.com";
-             var frompassword = "onlinestore###";
+            var response = await client.SendEmailAsync(msg);*/
+             var frommail = "rasheed.kub80@ethereal.email";
+             var frompassword = "2JXhyGDWgTEHwqxBQb";
              var message = new MailMessage();
              message.From = new MailAddress(frommail);
              message.Subject = subject;
              message.To.Add(email);
              message.Body = $"<html><body>{htmlMessage}</body></html>";
              message.IsBodyHtml = true;
-             var smtpClient = new SmtpClient("smtp.mail.outlook.com")
+            var Credentials = new NetworkCredential();
+            //Credentials.Password = frompassword;
+           // Credentials. = frommail;
+             var smtpClient = new SmtpClient(host: "smtp.ethereal.email")
              {
                  Port = 587,
-                 Credentials = new NetworkCredential(frommail, frompassword),
-                 EnableSsl = true
+                 Credentials = new NetworkCredential(),
+                 EnableSsl = true,
+                 
              };
-             smtpClient.Send(message);*/
+          //  smtpClient.
+             smtpClient.Send(message);
         }
     }
 }
